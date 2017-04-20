5a8b2a65667b342e8a7c662cd6cab2f2b033c554
Doug Franklin <doug@dougfranklin.com>
2017-04-20 05:25:40 -0400

    Karma works now

---
+++ src/test/magic-ball.spec.ts
@@ -0,0 +1,12 @@
+import { MagicBall } from 'src/providers/magic-ball';
+
+describe('Magic 8 Ball Service', () => {
+
+    it('should be evaluated as existing (or not)', () => {
+
+        expect(true).toBeTruthy();
+        // expect(magicBallService).toBeUndefined();
+
+    });
+
+});
