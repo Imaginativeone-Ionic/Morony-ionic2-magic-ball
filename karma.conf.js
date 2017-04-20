18a3bc1007a7774d0ef2b9038a82db2edf7eaf1c
Doug Franklin <doug@dougfranklin.com>
2017-04-20 05:26:40 -0400

    Karma works now :-)

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,28 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      { pattern: '.src/test.ts', watched: false }
+      {
+        /*// Detailed pattern to include a file. Similarly other options can be used
+        { pattern: 'lib/angular.js', watched: false },
+        // Prefer to have watched false for library files. No need to watch them for changes
+
+        // simple pattern to load the needed testfiles
+        // equal to {pattern: 'test/unit/*.spec.js', watched: true, served: true, included: true}
+        'test/unit/*.spec.js',
+
+        // this file gets served but will be ignored by the watcher
+        // note if html2js preprocessor is active, reference as `window.__html__['compiled/index.html']`
+        {pattern: 'compiled/index.html', watched: false},
+
+        // this file only gets watched and is otherwise ignored
+        {pattern: 'app/index.html', included: false, served: false},
+
+        // this file will be served on demand from disk and will be ignored by the watcher
+        {pattern: 'compiled/app.js.map', included: false, served: true, watched: false, nocache: true}*/ 
+
+        // pattern: '.src/test.ts', watched: false,
+        pattern: 'src/test/*.*', watched: true 
+      }
     ],
 
     // list of files to exclude
@@ -32,7 +53,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test/*.ts': ['angular-cli']
     },
 
     mime: {
